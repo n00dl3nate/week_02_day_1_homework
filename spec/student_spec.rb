271f088582f157117479feaca22ac52afa0eeb2f
oksana richards <oksana_vldk@hotmail.com>
2018-09-17 15:07:23 +0100

    check student name

---
+++ spec/student_spec.rb
@@ -0,0 +1,19 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../student')
+#
+class TestStudent < MiniTest::Test
+#
+def setup
+  @student1 = Student.new("Connor", "E22")
+  @student2 = Student.new("Susan", "E23")
+  @student3 = Student.new("Peter", "E21")
+end
+
+def test_student_name
+  result = @student1.get_name
+  assert_equal("Connor", result)
+end
+
+
+end
---
+++ student.rb
@@ -0,0 +1,15 @@
+class Student
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  def get_name
+    return @name
+  end
+
+def get_cohort
+  return @cohort
+end
+
+end
