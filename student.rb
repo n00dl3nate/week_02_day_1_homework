c2e313be6530d7039514fe3184819843c7fe09ef
oksana richards <oksana_vldk@hotmail.com>
2018-09-18 09:30:50 +0100

    exercise 1

--- student.rb
+++ student.rb
@@ -1,4 +1,6 @@
 class Student
+  attr_accessor :name, :cohort
+
   def initialize(name, cohort)
     @name = name
     @cohort = cohort
@@ -11,5 +13,14 @@ class Student
 def get_cohort
   return @cohort
 end
+
+def talk()
+  return "I can talk"
+end
+
+def say_favourite_language(language)
+  return "I love #{language}"
+end
+
 
 end
